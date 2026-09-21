be08d0847e7ebd676badfcd85ed2188a83d9ed56
Claes Engelin <claes.engelin@zocom.se>
2026-09-21 11:56:20 +0300

    Efter uppgift 6

---
+++ 06 Inloggningskontroll (Logiska operatorer)/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Användarnamn: ");
+string user = Console.ReadLine();
+Console.Write("Lösenord: ");
+string pass = Console.ReadLine();
+
+if (user == "admin" && pass == "hemligt123")
+{
+    Console.WriteLine("Inloggning lyckades!");
+}
+else
+{
+    Console.WriteLine("Felaktiga uppgifter.");
+}
+
+
+
+
+
+
+#region Specialkod på slutet
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
+#endregion
