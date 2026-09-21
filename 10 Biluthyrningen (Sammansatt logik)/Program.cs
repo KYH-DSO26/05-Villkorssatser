b55140e2c79d79af3e6174af649d04312c255acd
Claes Engelin <claes.engelin@zocom.se>
2026-09-21 13:28:11 +0300

    Efter uppgift 10

---
+++ 10 Biluthyrningen (Sammansatt logik)/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Ange din ålder: ");
+int ålder = int.Parse(Console.ReadLine());
+Console.Write("Hur många år har du haft körkort? ");
+int årKörkort = int.Parse(Console.ReadLine());
+Console.Write("Är du man eller kvinna? (m/k): ");
+string kön = Console.ReadLine().ToLower();
+
+if (kön == "m")
+{
+    if (ålder >= 25 && årKörkort >= 3)
+    {
+        Console.WriteLine("Du får hyra sportbilen!");
+    }
+    else
+    {
+        Console.WriteLine("Tyvärr, du uppfyller inte kraven.");
+    }
+}
+else
+{
+    if (ålder >= 23)
+        Console.WriteLine("Välkommen att hyra bilen!");
+
+    else
+    {
+        Console.WriteLine("Du är för ung, dessvärre!");
+    }
+}
+
+
+
+
+#region Specialkod på slutet
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
+#endregion
