15d6b336abc85acd55bb6fb2720ad2e373011f73
Claes Engelin <claes.engelin@zocom.se>
2026-09-21 12:10:04 +0300

    Efter uppgift 7

---
+++ 07 Rabattväljaren (Nästlade if)/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Är du student? (ja/nej): ");
+string student = Console.ReadLine().ToLower().Trim();
+
+if (student == "ja" || student == "j")
+{
+    Console.Write("Har du Mecenat-kort? (ja/nej): ");
+    string kort = Console.ReadLine().ToLower();
+    if (kort == "ja" || kort == "j")
+    {
+        Console.WriteLine("Du får 20% rabatt!");
+    }
+    else
+    {
+        Console.WriteLine("Du får 10% rabatt!");
+    }
+}
+else
+{
+    Console.WriteLine("Standarpris gäller.");
+}
+
+
+
+
+
+#region Specialkod på slutet
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
+#endregion
