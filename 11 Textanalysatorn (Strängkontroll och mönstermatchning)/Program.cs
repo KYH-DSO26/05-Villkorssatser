d34e670b1793bdc73015c35868ad101797c7ee61
Claes Engelin <claes.engelin@zocom.se>
2026-09-21 14:03:38 +0300

    Efter dagens lektion (vi slutade efter uppgift 11)

---
+++ 11 Textanalysatorn (Strängkontroll och mönstermatchning)/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Skriv en text: ");
+string text = Console.ReadLine();
+bool specialTextFinns = false;
+
+if (string.IsNullOrEmpty(text))
+{
+    Console.WriteLine("Texten är tom.");
+}
+if (text.StartsWith("Hejsan"))
+{
+    Console.WriteLine("Texten börjar med en hälsning.");
+    specialTextFinns = true;
+}
+if (text.EndsWith("!"))
+{
+    Console.WriteLine("Texten avslutas med utropstecken!");
+    specialTextFinns = true;
+}
+if (specialTextFinns == false)
+{
+    Console.WriteLine("Texten uppfyller inga specialvillkor");
+}
+
+
+
+
+
+
+#region Specialkod på slutet
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
+#endregion
