3b90b40dea0a47b279d2ad934bc82ccaa18f188d
Claes Engelin <claes.engelin@zocom.se>
2026-09-21 13:10:20 +0300

    Efter uppgift 9

---
+++ 09 Support-ärenden (Switch expressions - Switch med mönster)/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * För mer läsning, se https://www.geeksforgeeks.org/c-sharp/switch-expression-in-c-sharp-8-0/
+ */
+
+
+Console.Write("Ange felkod: ");
+int felkod = int.Parse(Console.ReadLine());
+
+string kategori = felkod switch
+{
+    >= 100 and <= 199 => "Hådvarufel",
+    >= 200 and <= 299 => "Mjukvarufel",
+    _ => "Okänt fel"
+};
+Console.WriteLine($"Kategori: {kategori}");
+
+
+
+
+
+
+#region Specialkod på slutet
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
+#endregion
