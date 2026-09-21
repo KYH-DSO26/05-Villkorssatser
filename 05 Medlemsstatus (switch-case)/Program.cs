ecee5803603423eb7839ced8484bdd740239713a
Claes Engelin <claes.engelin@zocom.se>
2026-09-21 11:38:28 +0300

    Efter uppgift 5

---
+++ 05 Medlemsstatus (switch-case)/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Mer läsning om hur switch-case fungerar:
+ * https://code-maze.com/csharp-switch-multiple-cases-return-same-result/
+ * Googla annars på "c# case with multiple alternatives"
+ */
+
+
+Console.Write("Ange din medlemsnivå (Basic/Standard/Premium): ");
+string nivå = Console.ReadLine();
+
+switch (nivå.ToLower().Trim())
+{
+	case "b":
+	case "basic": Console.WriteLine("Månadspris: 99 kr"); break;
+
+	case "s":
+	case "standard": Console.WriteLine("Månadspris: 149 kr"); break;
+
+	case "p":
+	case "premium": Console.WriteLine("Månadspris: 199 kr"); break;
+
+	default: Console.WriteLine("Okänd medlemsnivå."); break;
+}
+
+
+
+
+
+#region Specialkod på slutet
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
+#endregion
