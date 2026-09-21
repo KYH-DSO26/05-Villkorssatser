c3686106b5c2a4df83aae35b89e3009e2095408b
Claes Engelin <claes.engelin@zocom.se>
2026-09-21 10:43:34 +0300

    Efter uppgift 3

---
+++ 03 Temperaturvarnaren (if-else)/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Ange temperatur: ");
+double temp = double.Parse(Console.ReadLine());
+
+if (temp < 0.0)  // Om vi skriver 0.0 istället för bara 0 så blir koden tydligare
+{
+    Console.WriteLine("Det är minusgrader ute.");
+}
+else  // temp >= 0.0
+{
+    Console.WriteLine("Det är plusgrader eller nollgradigt ute.");
+}
+
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
