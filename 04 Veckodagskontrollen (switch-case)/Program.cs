2db6a14222e16a74657040a9dd2864bd3526ea2d
Claes Engelin <claes.engelin@zocom.se>
2026-09-21 11:04:51 +0300

    Efter uppgift 4

---
+++ 04 Veckodagskontrollen (switch-case)/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Ange ett heltal mellan 1 och 7: ");
+int dag = int.Parse(Console.ReadLine());
+
+switch (dag)
+{
+    case 1: Console.WriteLine("Måndag"); break;
+    case 2:   // Normalt vill man ha detta långa formatet, eftersom mer än en kodrad ska köras.
+		{
+            Console.WriteLine("Tisdag");
+			break;
+		}
+    case 3: Console.WriteLine("Onsdag"); break;
+    case 4: Console.WriteLine("Torsdag"); break;
+    case 5: Console.WriteLine("Fresdag"); break;
+    case 6: Console.WriteLine("Lördag"); break;
+    case 7: Console.WriteLine("Söndag"); break;
+    default:
+        Console.WriteLine("Felaktig inmatning! Ange ett tal mellan 1 och 7.");
+		break;
+}
+
+
+
+
+#region Specialkod på slutet
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
+#endregion
