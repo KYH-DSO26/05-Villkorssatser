edc396094b536f265e3596c74e74244c9b12fe8b
Claes Engelin <claes.engelin@zocom.se>
2026-09-21 10:03:15 +0300

    Add project files.

---
+++ 01 Åldersgränsen/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ange din ålder: ");
+int ålder = int.Parse(Console.ReadLine());
+
+if (ålder < 18)
+{
+    Console.WriteLine("Du är minderårig");
+}
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
---
+++ 02 Dörrvakten (if-else)/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Ange din ålder: ");
+int ålder = int.Parse(Console.ReadLine());
+
+// (a > b) samma sak som !(a <= b)
+// (a < b) är samma sak som !(a >= b)
+
+if (ålder >= 18)
+{
+    Console.WriteLine("Välkommen in!");
+}
+else
+{
+    Console.WriteLine("Du är minderårig");
+}
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
